a8bc249b12c4920143dc348d015a388faa661b80
Wang Hin Wu <henrywwh121@gmail.com>
2024-06-17 12:50:38 +0800

    'builtin_type'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 // Uncomment this block to pass the first stage
 // import java.util.Scanner;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    static String[] builtinCommands = {
+            "exit","type","echo"
+    };
+
     public static void main(String[] args) throws Exception {
 
         while(true) {
@@ -17,6 +23,16 @@ public class Main {
 
             if(input.startsWith("echo ")) {
                 System.out.println(input.substring(4).trim());
+            }
+            else if(input.startsWith("type ")) {
+                String typeToCheck = input.substring(4).trim();
+                boolean isBuiltin = Arrays.asList(builtinCommands).contains(typeToCheck);
+                if(isBuiltin) {
+                    System.out.println(typeToCheck + " is a shell builtin");
+                }
+                else {
+                    System.out.println(typeToCheck + ": not found");
+                }
             }
             else {
                 System.out.println(input + ": command not found");
