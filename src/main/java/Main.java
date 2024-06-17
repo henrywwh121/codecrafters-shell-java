c59bdb67c5f5ee344e5db149c76c89a83ae88f37
Wang Hin Wu <henrywwh121@gmail.com>
2024-06-17 12:20:08 +0800

    'exit'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,20 @@
 // Uncomment this block to pass the first stage
 // import java.util.Scanner;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-
-        // Uncomment this block to pass the first stage
+        
         while(true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+
+            if(Objects.equals(input, "exit 0")) {
+                break;
+            }
             System.out.println(input + ": command not found");
         }
     }
