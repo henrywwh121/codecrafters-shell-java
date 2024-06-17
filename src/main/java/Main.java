c4a492e33ccf9fa08349351b46711529a2e4dec6
Wang Hin Wu <henrywwh121@gmail.com>
2024-06-17 12:27:22 +0800

    'echo_builtin'

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,26 @@
 // Uncomment this block to pass the first stage
 // import java.util.Scanner;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+
         while(true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
 
-            if(Objects.equals(input, "exit 0")) {
+            if(input.equals("exit 0")) {
                 break;
             }
-            System.out.println(input + ": command not found");
+
+            if(input.startsWith("echo ")) {
+                System.out.println(input.split(" ")[1]);
+            }
+            else {
+                System.out.println(input + ": command not found");
+            }
         }
     }
 }
